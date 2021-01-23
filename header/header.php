f9fbd106b115c5c356ab50115d99cf92ab6d8bb7
Sandali Boteju <66761408+sandalib@users.noreply.github.com>
2021-01-23 18:38:48 +0530

    Add files via upload
    Please edit these files and use.

---
+++ header/header.php
@@ -0,0 +1,51 @@
+<html>
+	<head>
+		<title>
+			header
+		</title>
+		<link rel="stylesheet" href="CSS/header.css">
+	</head>
+	<body>
+	<div id="box">
+		<div class="row">
+			
+			<div class="column section" id="home" >
+				<a href="menu.php">
+				<p>Home</p>
+				</a>
+			</div>
+			
+			<div class="column section" id="student">
+				<a href="Student.php">
+				<p>Student</p>
+				</a>
+			</div>
+			
+			
+			<div class="column section" id="course">
+				<a href="course.php">
+				<p>Course</p>
+				</a>
+			</div>
+			
+			<div class="column section" id="subject">
+				<a href="subject.php">
+				<p>Subject</p>
+				</a>
+			</div>
+			
+			<div class="column section" id="attendence">
+				<a href="attendence.php">
+				<p>Attendence</p>
+				</a>
+			</div>
+			
+			<div class="column section" id="stco">
+				<a href="studentcourse.php">
+				<p>Studentcourse</p>
+				</a>
+			</div>
+		</div>
+	</div>
+	</body>
+</html>
